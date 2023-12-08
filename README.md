84e61eb050ecda367b74f9deb59920b477a40a5e
VikramThakorXplor <vikramji.thakor@xplortechnologies.com>
2023-12-08 11:11:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RecurringBookingApp
+ 
---
+++ RecurringBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecurringBookingAppApp.swift
+//  RecurringBookingApp
+//
+//  Created by Vikram Thakor on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RecurringBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
